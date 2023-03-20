76098bc304867dccbbdf8f55d829a3a75b43da62
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-03-20 14:56:12 +0100

    inicial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log('May Node be with you')
+const express = require('express')
+const bodyParser = require('body-parser') //trebalo bi da moze i bez ovoga
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true})) //trebalo bi da moze i bez ovoga
+
+app.listen(3000, function(){
+    console.log('listening on 3000')
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res)=> {
+    console.log(req.body)
+})

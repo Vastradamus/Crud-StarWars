a9a1143a58c303bedff556a0df828b0b21eef4e8
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-03-21 10:12:03 +0100

    added index.ejs

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         quotesCollection.find().toArray()
             .then(results => {
                 console.log(results)
+                res.render('index.ejs', {quotes:results})
             })
             .catch(error => console.error(error))
-        res.sendFile(__dirname + '/index.html')
+            
     })
     app.post('/quotes', (req, res)=> {
        quotesCollection.insertOne(req.body)

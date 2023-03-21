a84c29b090867a5c4db2c4d09e22db441147fa11
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-03-21 09:49:26 +0100

    connecting to MongoDB

--- server.js
+++ server.js
@@ -2,17 +2,44 @@ console.log('May Node be with you')
 const express = require('express')
 const bodyParser = require('body-parser') //trebalo bi da moze i bez ovoga
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://Vasa:QDhI34oYI8VIyByz@cluster11.tf3awez.mongodb.net/?retryWrites=true&w=majority'
 
-app.use(bodyParser.urlencoded({extended: true})) //trebalo bi da moze i bez ovoga
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({extended: true})) //trebalo bi da moze i bez ovoga
+    app.get('/', (req,res) => {
+        quotesCollection.find().toArray()
+            .then(results => {
+                console.log(results)
+            })
+            .catch(error => console.error(error))
+        res.sendFile(__dirname + '/index.html')
+    })
+    app.post('/quotes', (req, res)=> {
+       quotesCollection.insertOne(req.body)
+       .then(result => {
+        console.log(result)
+        res.redirect('/')
+       })
+       .catch(error => console.error(error))
+    })
+    app.listen(3000, function(){
+        console.log('listening on 3000')
+    })
+    
 
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
+  })
+  .catch(error => console.error(error))
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
 
-app.post('/quotes', (req, res)=> {
-    console.log(req.body)
-})
+
+
+
+
+
+
